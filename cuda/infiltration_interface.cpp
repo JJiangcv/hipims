5d362d76e801c3d0fb26f9ff6373dbd8fdc10c1e
JJiangcv <jinghua.jiang21@gmail.com>
2025-01-30 11:39:51 +0000

    add sewer_sink parameter

--- cuda/infiltration_interface.cpp
+++ cuda/infiltration_interface.cpp
@@ -1,3 +1,13 @@
+/*** 
+ * @Author: JJiangcv jinghua.jiang21@gmail.com
+ * @Date: 2021-09-02 15:53:53
+ * @LastEditors: JJiangcv jinghua.jiang21@gmail.com
+ * @LastEditTime: 2025-01-30 11:39:31
+ * @FilePath: /cvjj7/hipims/cuda/infiltration_interface.cpp
+ * @Description: 
+ * @
+ * @Copyright (c) 2025 by ${git_name_email}, All Rights Reserved. 
+ */
 #include <torch/extension.h>
 #include <vector>
 
@@ -7,8 +17,9 @@ void infiltrationCalculation_cuda(at::Tensor wetMask, at::Tensor h_update,
                                   at::Tensor hydraulic_conductivity,
                                   at::Tensor capillary_head,
                                   at::Tensor water_content_diff,
-                                  at::Tensor cumulative_depth, at::Tensor dt);
-
+                                  at::Tensor cumulative_depth,
+                                  at::Tensor sewer_sink,
+                                  at::Tensor dt);
 // C++ interface
 #define CHECK_CUDA(x)                                                          \
   TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor. ")
@@ -23,7 +34,11 @@ void infiltrationCalculation(at::Tensor wetMask, at::Tensor h_update,
                              at::Tensor hydraulic_conductivity,
                              at::Tensor capillary_head,
                              at::Tensor water_content_diff,
-                             at::Tensor cumulative_depth, at::Tensor dt) {
+                             at::Tensor cumulative_depth,
+                             at::Tensor sewer_sink,
+                             at::Tensor dt)
+
+{
   CHECK_INPUT(wetMask);
   CHECK_INPUT(landuse);
   CHECK_INPUT(h);
@@ -33,10 +48,11 @@ void infiltrationCalculation(at::Tensor wetMask, at::Tensor h_update,
   CHECK_INPUT(capillary_head);
   CHECK_INPUT(water_content_diff);
   CHECK_INPUT(cumulative_depth);
+  CHECK_INPUT(sewer_sink);
 
   infiltrationCalculation_cuda(wetMask, h_update, landuse, h,
                                hydraulic_conductivity, capillary_head,
-                               water_content_diff, cumulative_depth, dt);
+                               water_content_diff, cumulative_depth, sewer_sink, dt);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
